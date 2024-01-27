db989ac1fe95c4cb58eac9253c1dc3be8ed99818
bsensix <breno@sensix>
2024-01-27 17:57:49 -0300

    add: emoji trafego pago

--- StPages.py
+++ StPages.py
@@ -32,7 +32,7 @@ show_pages(
         ),
         Page("Analise Descritiva de Dados.py", "Análise Descritiva de Dados", "📊"),
         Page("Dados NBA.py", "Previsão de Total Pontos NBA", "🏀"),
-        Page("Trafego Pago.py", "Análise Dados Tráfego Pago", "ⓕ"),
+        Page("Trafego Pago.py", "Análise Dados Tráfego Pago", "🧑‍💻"),
     ]
 )
 
