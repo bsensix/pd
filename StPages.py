f8e2b1432d9e818a8ce3a720efd2a13d65b2562c
Breno Nunes <109923359+bsensix@users.noreply.github.com>
2024-06-10 17:07:29 -0300

    Update StPages.py

--- StPages.py
+++ StPages.py
@@ -1,28 +1,23 @@
-from st_pages import Page, Section, add_page_title, show_pages
+from st_pages import Page, add_page_title, show_pages
 
 show_pages(
     [
         Page("Sobre Mim.py", "Sobre Mim", "🏠"),
-        Section(name="🛰️ Geoprocessamento"),
         Page("Mapas.py", "Mapas", "🗺️"),
         Page(
             "Página - GEE - Download de Imagens.py", "Download de Imagens no GEE", "🌎"
         ),
-        Section(name="📷 Visão Computacional"),
         Page("Classificador_feijoes.py", "Feijões Estragados - Tensor Flow", "🫘"),
         Page(
-            "Identificação de Pivôs - Interface.py",
-            "Identificação de Círculos (OpenCV)",
-            "🔘",
+            "Identificação de Pivôs - Interface.py", "Identificação de Círculos", "🔘"
         ),
-        Section(name="🎲 Análise de dados"),
         Page("Poluicao_ar.py", "Poluição do Ar (OpenWeather)", "🌫️"),
         Page("Analise Descritiva de Dados.py", "Análise Descritiva de Dados", "📊"),
         Page("Dados NBA.py", "Previsão de Total Pontos NBA", "🏀"),
-        Section(name="📈 Dashboards"),
-        Page("Trafego Pago.py", "Dados Tráfego Pago (Looker)", "🧑‍💻"),
+        Page("Trafego Pago.py", "Análise Dados Tráfego Pago (Looker)", "🧑‍💻"),
         Page("Incendios.py", "Dados de Queimadas (Power BI)", "🔥"),
     ]
 )
 
 add_page_title()  # Optional method to add title and icon to current page
+
